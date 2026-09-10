4bd3922fa0967858bd2b88749de4e7188d456933
krish <144285882+krishj004@users.noreply.github.com>
2026-09-10 17:47:05 +0530

    Time: 1 ms (25.99%), Space: 44 MB (30.28%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void merge(int[] nums1, int n, int[] nums2, int m) {
+        int length=(n+m);
+        int gap=(length/2)+(length%2);
+        while(gap>0){
+            int left=0;
+            int right=left+gap;
+            while(right<length){
+                if(left<n && right>=n){
+                    swapgreater(nums1,nums2,left,right-n);
+                }
+                else if(left>=n){
+                    swapgreater(nums2,nums2,left-n,right-n);
+
+                }
+                else{
+                    swapgreater(nums1,nums1,left,right);
+
+                }
+                left++; right++;
+                
+            }
+            if(gap==1) break;
+                gap=(gap/2)+(gap%2);
+
+        }
+        for(int i=0;i<m;i++){
+            nums1[n+i] = nums2[i];
+        }
+    }
+    public void swapgreater(int[] arr1,int[] arr2,int n,int m){
+        if(arr1[n]>arr2[m]){
+            int temp=arr1[n];
+            arr1[n]=arr2[m];
+            arr2[m]=temp;
+        }
+    }
+}
