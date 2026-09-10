9c4092bd35b2960597cb35bcade8c00ac1f55c8f
krish <144285882+krishj004@users.noreply.github.com>
2026-09-10 16:45:37 +0530

    Time: 11 ms (61.98%), Space: 57.3 MB (22.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,9 +2,24 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
-        for(int i=0;i<n;i++)
-        Arrays.fill(dp[i],-1);
-        return fn(coins,n-1,amount,dp);
+       // for(int i=0;i<n;i++)
+       // Arrays.fill(dp[i],-1);
+        //return fn(coins,n-1,amount,dp);
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)
+            dp[0][i]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int nottaker=dp[i-1][j];
+                int take=0;
+                if(coins[i]<=j)
+                take=dp[i][j-coins[i]];
+                dp[i][j]=(nottaker+take);  
+            }
+        }
+        return dp[n-1][amount];
+
     }
     public int fn(int[] coins,int ind,int target,int[][] dp){
         if(ind==0)
